1ac3f5decd9d53ae6daaf7a4aa3dd368948cbd8b
Rithin Rajen <77372858+rithtinsk@users.noreply.github.com>
2025-03-20 05:50:47 +0530

    predict_help.py

--- predict_help.py
+++ predict_help.py
@@ -34,7 +34,7 @@ def calculate_normalized_risk(medical_history):
 def preprocess_input(input_dict):
     # Define the expected columns and initialize the DataFrame with zeros
     expected_columns = [
-        'age', 'number_of_dependants', 'income_lakhs', 'insurance_plan', 'genetical_risk', 'normalized_risk_score',
+        'age', 'number_of_dependants', 'income_lakhs', 'insurance_plan', 'genetical_risk', 'normalized_score',
         'gender_Male', 'region_Northwest', 'region_Southeast', 'region_Southwest', 'marital_status_Unmarried',
         'bmi_category_Obesity', 'bmi_category_Overweight', 'bmi_category_Underweight', 'smoking_status_Occasional',
         'smoking_status_Regular', 'employment_status_Salaried', 'employment_status_Self-Employed'
